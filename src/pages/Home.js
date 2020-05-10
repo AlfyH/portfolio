27417b2c3da5438c2c4fbf331e891262d54398e1
Alfy Hushairi <alfy.hushairi@accedo.tv>
2020-05-10 16:53:12 +0100

    rabbit image as fallback

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ class Home extends Component {
                         <Nav></Nav>
                     </div>
                     <div id="rabbit-hero-cont">
-                        {/* <img id="rabbit-hero" src={rabbithero} alt ="rabbit-hero.png"></img> */}
+                        <img id="rabbit-hero" src={rabbithero} alt ="rabbit-hero.png"></img>
                         <video id="rabbit-hero" src='https://res.cloudinary.com/alfy/video/upload/v1589122995/yellowRabbit_aibfsh.mov' autoPlay loop muted playsInline></video>
                     </div>
                     <div id="text-hero-cont">
